df47bab1a7620ea827a0eaa62ffe072f4aac47b2
AnantVijaySingh <anantvijay@oath.com>
2018-07-01 23:53:12 +0100

    Metric Card Component Issue: AsyncStorage does not set the state by default needs to be passed default once to get it going.

--- components/History.js
+++ components/History.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, Platform, TouchableOpacity} from 'react-native';
 import {connect} from 'react-redux';
 import {receiveEntries, addEntry} from "../actions";
 import {timeToString, getDailyReminderValue} from "../utils/helpers";
 import {fetchCalendarResults} from "../utils/api";
 import UdaciFitnessCalendar from 'udacifitness-calendar';
+import {white} from "../utils/colors";
+import DateHeader from './DateHeader';
+import MetricCard from './MetricCard';
+import {AppLoading} from 'expo';
 
 class History extends React.Component {
+
+    state = {
+        ready: false
+    };
 
     componentDidMount() {
         const {dispatch} = this.props;
@@ -20,28 +28,46 @@ class History extends React.Component {
                     }))
                 }
             })
+            .then(() => {
+                this.setState({
+                    ready: true
+                })
+            })
     }
 
     renderItem = ({today, ...metrics}, formattedDate, Key) => (
-      <View>
+      <View style={styles.item}>
           {
               today ?
-                  <Text>{JSON.stringify(today)}</Text> :
-                  <Text>{JSON.stringify(metrics)}</Text>
+                  <View>
+                      <DateHeader date={formattedDate} />
+                      <Text style={styles.noDataText}>{today}</Text>
+                  </View> :
+                  <TouchableOpacity>
+                      <MetricCard metrics={metrics} date={formattedDate}/>
+                  </TouchableOpacity>
           }
       </View>
     );
 
     renderEmptyDate (formattedDate) {
         return (
-            <View>
-                <Text>No data for this day</Text>
+            <View style={styles.item}>
+                <DateHeader date={formattedDate}/>
+                <Text style={styles.noDataText} > No data for this day</Text>
             </View>
         )
     };
 
     render() {
         const {entries} = this.props;
+        const {ready} = this.state;
+
+        if(ready === false) {
+            return (
+                <AppLoading />
+            )
+        }
 
         return (
             <UdaciFitnessCalendar
@@ -52,6 +78,28 @@ class History extends React.Component {
         )
     }
 }
+
+const styles = StyleSheet.create({
+   item: {
+       backgroundColor: white,
+       borderRadius: Platform.OS === 'ios' ? 16 : 2,
+       margin: 10,
+       padding: 20,
+       justifyContent: 'center',
+       shadowRadius: 3,
+       shadowOpacity: 0.8,
+       shadowColor: 'rgba(0,0,0,0.8)',
+       shadowOffset: {
+           width: 0,
+           height: 3
+       }
+   },
+   noDataText: {
+        fontSize: 20,
+       paddingBottom: 20,
+       paddingTop: 20
+   }
+});
 
 function mapStateToProps(entries) {
     return {
